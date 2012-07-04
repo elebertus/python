55c68cf1f5e427a869920422c0bc12cd08ccb2b4
elebertus <eblack82@gmail.com>
2012-07-03 18:36:39 -0700

    changed to tmp_python

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,5 @@
+# http://tickets.opscode.com/browse/COOK-1405 once this is built switch back to python
+# in https://github.riotgames.com/cookbooks/DWDBaseline/blob/master/Berksfile
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
@@ -7,11 +9,11 @@ version           "1.0.6"
 depends           "build-essential"
 depends           "yum"
 
-recipe "python", "Installs python, pip, and virtualenv"
-recipe "python::package", "Installs python using packages."
-recipe "python::source", "Installs python from source."
-recipe "python::pip", "Installs pip from source."
-recipe "python::virtualenv", "Installs virtualenv using the python_pip resource."
+recipe "tmp_python", "Installs python, pip, and virtualenv"
+recipe "tmp_python::package", "Installs python using packages."
+recipe "tmp_python::source", "Installs python from source."
+recipe "tmp_python::pip", "Installs pip from source."
+recipe "tmp_python::virtualenv", "Installs virtualenv using the python_pip resource."
 
 %w{ debian ubuntu centos redhat fedora freebsd }.each do |os|
   supports os
